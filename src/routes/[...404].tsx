e91cbfcc918597aa51b7834cc826d9fc95ae76b4
3add <141489004+3add@users.noreply.github.com>
2024-01-12 12:05:34 +0100

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,13 +6,13 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
       <p>
-        Visit{" "}
+        {" "}
         <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+          Back to home page
         </a>{" "}
-        to learn how to build SolidStart apps.
       </p>
     </main>
   );
